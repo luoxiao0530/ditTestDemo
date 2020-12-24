6b87ba75c27d08805f93295a846e26df19dd8e6b
86177 <CPHbusters0530>
2020-12-24 15:14:59 +0800

    这是我的第二次新增的老师类

---
+++ src/com/yidu/bean/Teacher.java
@@ -0,0 +1,13 @@
+package com.yidu.bean;
+
+/**
+ * @author
+ * @version 1.0
+ * @description:
+ * @adte 2020/12/24 15:12
+ */
+public class Teacher {
+    public static void main(String[] args) {
+        System.out.println("提交了两次");
+    }
+}
