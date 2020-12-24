5e5481b2fd41a3c7383258bb17f088becc3b246f
86177 <CPHbusters0530>
2020-12-24 15:08:33 +0800

    这是我得第一次提交

---
+++ src/com/yidu/bean/Student.java
@@ -0,0 +1,13 @@
+package com.yidu.bean;
+
+/**
+ * @author
+ * @version 1.0
+ * @description:
+ * @adte 2020/12/24 14:53
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("hello,word!");
+    }
+}
